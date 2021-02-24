2fa8ffe71dd59f48964d9d710059787541fb7a96
Darshak11 <Darshak.Jivrajani@iiitb.org.com>
2021-02-24 16:19:41 +0530

    commented icoheaderreader

--- icoheaderreader.c
+++ icoheaderreader.c
@@ -1,7 +1,8 @@
 #include "icoheaderreader.h"
 struct ICO_header Icoheaderreader(FILE *fp)
 {
-	struct ICO_header header;
-	fread(&header,6,1,fp);
-	return header;
+	struct ICO_header header;// creating a variable named header of the user-defined data type, struct ICO_header
+	fread(&header,6,1,fp);//this function fread has 4 parameters, (pointer,size in bytes of each element to be read,number of 
+                              //elements,pointer to a FILE object that specifies an input stream)
+	return header;//returning header variable with 3 elements of 2 byte size each
 }
--- icoheaderreader.h
+++ icoheaderreader.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
-struct ICO_header
+struct ICO_header   //ICO file consists of an Icon header and an Icon Directory 
+
+//Icon Header stores general information about the ICO file
+//This is the Header
 { 
-	unsigned short constant;
-	unsigned short imgt;
-	unsigned short nimages;
+	unsigned short constant; //this constant is Reserved and is of size 2 bytes and must be 0 always
+	unsigned short imgt;     //this imgt is Type, specifies the image type, 1 for icon (.ICO) image
+	unsigned short nimages;  //this is the Count, specifies the number of images in the file
 };
-struct ICO_header Icoheaderreader(FILE *);
+struct ICO_header Icoheaderreader(FILE *);//prototype declairation
