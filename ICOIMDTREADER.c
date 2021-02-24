b9a60d7f84aaa704dabde191d2f61101c3e4c717
Darshak11 <Darshak.Jivrajani@iiitb.org.com>
2021-02-24 16:17:53 +0530

    commented ICOIMDREADER

--- ICOIMDTREADER.c
+++ ICOIMDTREADER.c
@@ -1,15 +1,15 @@
 #include "ICOIMDTREADER.h"
 struct imagedirectory Imagedirectoryreader(FILE *fp)
 {
-	struct imagedirectory img;
-	fread(&img.width,1,1,fp);
-	fread(&img.height,1,1,fp);
-	fread(&img.cc,1,1,fp);
-	fread(&img.reserve,1,1,fp);
-	fread(&img.cp,2,1,fp);			//fread(&img.cp,4,1,fp);
+	struct imagedirectory img;			//making a varible of imagedirectory structure
+	fread(&img.width,1,1,fp);			// fread( ptr, size_bytes, n_members, FilePtr);		
+	fread(&img.height,1,1,fp);			// ptr = "pointer to a block of memory" 
+	fread(&img.cc,1,1,fp);				// size_bytes = "size in bytes of each element to be read"
+	fread(&img.reserve,1,1,fp);			// n_members = "number of elements, with size mentioned before"
+	fread(&img.cp,2,1,fp);				// FilePtr = "pointer to a FILE object that specifies an input"
 	fread(&img.bpp,2,1,fp);
 	fread(&img.bmpsize,4,1,fp);
 	fread(&img.offset,4,1,fp);
-	fseek(fp,img.offset,SEEK_SET);
+	fseek(fp,img.offset,SEEK_SET);		// fseek("pointer to a file", "no. of byte to offset", "Beginning of file")
 	return img;
 }
--- ICOIMDTREADER.h
+++ ICOIMDTREADER.h
@@ -1,13 +1,13 @@
 #include <stdio.h>
-struct imagedirectory
+struct imagedirectory			//on a whole takes 16 bytes
 {
-	unsigned char width;
-	unsigned char height;
-	unsigned char cc;
-	unsigned char reserve;
-	unsigned short cp;
-	unsigned short bpp;
-	unsigned int bmpsize;
-	unsigned int offset;
+	unsigned char width;		//takes 1 byte
+	unsigned char height;		//takes 1 byte
+	unsigned char cc;			//takes 1 byte
+	unsigned char reserve;		//takes 1 byte
+	unsigned short cp;			//takes 2 byte
+	unsigned short bpp;			//takes 2 byte
+	unsigned int bmpsize;		//takes 4 byte
+	unsigned int offset;		//takes 4 byte
 };
 struct imagedirectory Imagedirectoryreader(FILE *);
