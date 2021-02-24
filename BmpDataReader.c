b061fd873f0c325c639e2d55357103c21a205a2d
Darshak11 <Darshak.Jivrajani@iiitb.org.com>
2021-02-24 15:00:45 +0530

    commenting the bmp data and header functions

--- BmpDataReader.c
+++ BmpDataReader.c
@@ -1,31 +1,26 @@
 #include "BmpDataReader.h"
 #include <stdlib.h>
-struct pic bmp32reader(FILE *fp,unsigned int height,unsigned int width)
+struct pic bmp32reader(FILE *fp,unsigned int height,unsigned int width)//Function returning the the full image data with the height and width of the image
 {
 	struct pic img;
-	
-	img.height=height;	
-	if(img.height==0)
-		img.height=256;
+	img.height=height;
 	img.width=width;
-	if(img.width==0)
-		img.width=256;
-	img.rgba=(struct RGBA**) malloc(img.height*sizeof(void*));
+	img.rgba=(struct RGBA**) malloc(img.height*sizeof(void*));//Dynamically assigning memory to pointer pointer from the structure of image, in simpler words assigning number of rows to the 2d dimensional picture array
 	for(int i=img.height-1;i>=0;i--)
 	{
-		img.rgba[i]=(struct RGBA*)malloc(img.width*sizeof(struct RGBA));
+		img.rgba[i]=(struct RGBA*)malloc(img.width*sizeof(struct RGBA));//Dynamically assigning memory to the pointer of the pointer pointer or assigning the memory for the width of the image data
 		for(int j=0;j<img.width;j++)
 		{
-			fread(&img.rgba[i][j],4,1,fp);
+			fread(&img.rgba[i][j],4,1,fp);//Reading each pixel from the data
 		}
 	}
 	return img; 
 }
-void freeImage(struct pic img)
+void freeImage(struct pic img)//Function to free the dynamically assigned memory
 {
 	for(int i=img.height-1;i>=0;i--)
 	{
-		free(img.rgba[i]);
+		free(img.rgba[i]);//Freeing each row of the 2d array at a time,freeing the columns
 	}
-	free(img.rgba);	
+	free(img.rgba);//freeing the complete allocated memory at last, freeing the rows
 }
--- BmpHeaderreader.c
+++ BmpHeaderreader.c
@@ -1,9 +1,9 @@
 #include "BmpHeaderreader.h"
-struct BMPData readbmp(FILE *ifile)
+struct BMPData readbmp(FILE *ifile)//Function returning a structure which will have the BMPDIB header in it
 {
 	struct BMPData Data;
 
-	fread(&Data.intBitmapheadersize,4,1,ifile);		
+	fread(&Data.intBitmapheadersize,4,1,ifile);//Reading the data from the file into header data according to their size and the fixed structure of a BMPDIB header 	
 	fread(&Data.intWidth,4,1,ifile);				
 	if (Data.intWidth == 0)
 		Data.intWidth = 256;
